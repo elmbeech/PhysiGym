1c54a07776465e1bc3dd3b27617e13825c0a15c3
bue <epbucher@iu.edu>
2025-07-13 21:52:05 -0400

    @ tibbue : on my way.

--- model/tibbue/custom_modules/physigym/physicell_model.py
+++ model/tibbue/custom_modules/physigym/physicell_model.py
@@ -392,7 +392,7 @@ class ModelPhysiCellEnv(CorePhysiCellEnv):
             cost function.
         """
         if (self.c_prev > 0):
-            r_reward_tumor = (self.c_prev - self.c_t) / (self.c_prev * np.e**(physicell.get_parameter("r_growth") * physicell.get_parameter("dt_gym")) - self.c_prev)
+            r_reward_tumor = (self.c_prev - self.c_t) / (self.c_prev * np.e**(physicell.get_parameter("growth_rate") * physicell.get_parameter("dt_gym")) - self.c_prev)
             r_reward_tumor = np.clip(r_reward_tumor, -1, 1)
         else:
             r_reward_tumor = None
