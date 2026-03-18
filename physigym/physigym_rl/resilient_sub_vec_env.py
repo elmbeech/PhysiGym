a8f07f04453ced94d28e17dfc0f4f0467ecfc087
Dante-Berth <bertinalexAI@protonmail.com>
2026-03-18 23:57:17 +0100

    [UPDATE] new resilient vec env

--- physigym/physigym_rl/resilient_sub_vec_env.py
+++ physigym/physigym_rl/resilient_sub_vec_env.py
@@ -1,13 +1,32 @@
 import multiprocessing as mp
 import numpy as np
 from typing import Set
-
-from stable_baselines3.common.vec_env.subproc_vec_env import (
-    SubprocVecEnv,
-    _stack_obs,
-)
+from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv, _stack_obs
+from gymnasium import spaces
 
 
+# ------------------------------------------------------------------
+# Dummy env used to replace crashed ones safely
+# ------------------------------------------------------------------
+class DummyEnv:
+    """Safe placeholder for crashed environments."""
+
+    def __init__(self, observation_space):
+        self.observation_space = observation_space
+        self.action_space = None
+
+    def step(self, action):
+        obs = self.observation_space.sample()
+        return obs, 0.0, True, {"crashed": True, "disabled": True, "step_episode": -1}
+
+    def reset(self, seed=None, options=None):
+        obs = self.observation_space.sample()
+        return obs, {"crashed": True, "disabled": True, "step_episode": -1}
+
+
+# ------------------------------------------------------------------
+# Resilient VecEnv
+# ------------------------------------------------------------------
 class ResilientSubprocVecEnv(SubprocVecEnv):
     """
     SubprocVecEnv variant that permanently disables crashing environments
@@ -19,6 +38,7 @@ class ResilientSubprocVecEnv(SubprocVecEnv):
 
         self.env_fns = env_fns
         self.dead_envs: Set[int] = set()
+        self._dummy_envs = {}
 
         super().__init__(env_fns, start_method=start_method)
 
@@ -29,13 +49,11 @@ class ResilientSubprocVecEnv(SubprocVecEnv):
     # ------------------------------------------------------------------
     # Crash handling
     # ------------------------------------------------------------------
-
     def _disable_env(self, i: int):
         if i in self.dead_envs:
             return
 
         print(f"[ResilientVecEnv] Disabling env {i}")
-
         self.dead_envs.add(i)
 
         try:
@@ -49,10 +67,34 @@ class ResilientSubprocVecEnv(SubprocVecEnv):
         except Exception:
             pass
 
+        # Create dummy env for safe stepping
+        self._dummy_envs[i] = DummyEnv(self.env_fns[i]().observation_space)
+
+    # ------------------------------------------------------------------
+    # Safe set_attr: skips dead envs
+    # ------------------------------------------------------------------
+    def set_attr(self, attr_name, value, indices=None):
+        indices = indices if indices is not None else range(self.num_envs)
+        for i in indices:
+            if i in self.dead_envs:
+                continue
+            super().set_attr(attr_name, value, [i])
+
+    # ------------------------------------------------------------------
+    # Safe env_method: skips dead envs
+    # ------------------------------------------------------------------
+    def env_method(self, method_name, *method_args, indices=None, **method_kwargs):
+        indices = indices if indices is not None else range(self.num_envs)
+        safe_indices = [i for i in indices if i not in self.dead_envs]
+        if not safe_indices:
+            return []
+        return super().env_method(
+            method_name, *method_args, indices=safe_indices, **method_kwargs
+        )
+
     # ------------------------------------------------------------------
     # Step
     # ------------------------------------------------------------------
-
     def step_async(self, actions):
         for i, (remote, action) in enumerate(zip(self.remotes, actions)):
             if i in self.dead_envs:
@@ -65,35 +107,15 @@ class ResilientSubprocVecEnv(SubprocVecEnv):
 
         for i, remote in enumerate(self.remotes):
             if i in self.dead_envs:
-                obs = self.observation_space.sample()
-                reward = 0.0
-                done = True
-
-                info = {
-                    "crashed": True,
-                    "disabled": True,
-                    "terminal_observation": obs,
-                    "step_episode": -1,
-                }
-
+                obs, reward, done, info = self._dummy_envs[i].step(None)
                 results.append((obs, reward, done, info, info))
                 continue
 
             try:
                 results.append(remote.recv())
-            except (EOFError, BrokenPipeError):
+            except (EOFError, BrokenPipeError, OSError):
                 self._disable_env(i)
-
-                obs = self.observation_space.sample()
-                reward = 0.0
-                done = True
-
-                info = {
-                    "crashed": True,
-                    "disabled": True,
-                    "step_episode": -1,
-                }
-
+                obs, reward, done, info = self._dummy_envs[i].step(None)
                 results.append((obs, reward, done, info, info))
 
         self.waiting = False
@@ -109,36 +131,27 @@ class ResilientSubprocVecEnv(SubprocVecEnv):
     # ------------------------------------------------------------------
     # Reset
     # ------------------------------------------------------------------
-
     def reset(self):
         for i, remote in enumerate(self.remotes):
             if i in self.dead_envs:
                 continue
-            remote.send(("reset", (self._seeds[i], self._options[i])))
+            try:
+                remote.send(("reset", (self._seeds[i], self._options[i])))
+            except (OSError, EOFError, BrokenPipeError):
+                self._disable_env(i)
 
         results = []
         for i, remote in enumerate(self.remotes):
             if i in self.dead_envs:
-                obs = self.observation_space.sample()
-                reset_info = {
-                    "crashed": True,
-                    "disabled": True,
-                    "step_episode": -1,
-                }
+                obs, reset_info = self._dummy_envs[i].reset()
                 results.append((obs, reset_info))
                 continue
 
             try:
                 results.append(remote.recv())
-            except (EOFError, BrokenPipeError):
+            except (EOFError, BrokenPipeError, OSError):
                 self._disable_env(i)
-
-                obs = self.observation_space.sample()
-                reset_info = {
-                    "crashed": True,
-                    "disabled": True,
-                    "step_episode": -1,
-                }
+                obs, reset_info = self._dummy_envs[i].reset()
                 results.append((obs, reset_info))
 
         obs, self.reset_infos = zip(*results)
