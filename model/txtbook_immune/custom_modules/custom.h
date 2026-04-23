97174657a39362640c67d571c09e9ca6305231ed
bue <epbucher@iu.edu>
2026-04-22 21:30:52 -0400

    @ txtbook_immune : we are rolling.

--- model/txtbook_immune/custom_modules/custom.h
+++ model/txtbook_immune/custom_modules/custom.h
@@ -5,6 +5,7 @@
 // date: 2015-2024
 // license: BSD-3-Clause
 // author: Elmar Bucher, Paul Macklin
+//
 // original source code: https://github.com/MathCancer/PhysiCell
 // modified source code: https://github.com/elmbeech/physicellembedding
 ////////
@@ -28,6 +29,10 @@ std::vector<std::string> my_coloring_function(Cell*);
 
 // custom functions can go here
 void phenotype_function(Cell* pCell, Phenotype& phenotype, double dt);
+void efferocytosis( double dt );  // bue 20251014
 void custom_function(Cell* pCell, Phenotype& phenotype, double dt);
 void contact_function(Cell* pMe, Phenotype& phenoMe, Cell* pOther, Phenotype& phenoOther, double dt);
 
+// implementations.
+void cell_arrival_function(double dt);
+void cell_departure_function(double dt);
